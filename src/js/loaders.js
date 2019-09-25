02bda4ada7331d85139869ec322b0e51fbbc77b2
Janek <janzborowski99@gmail.com>
2019-09-25 18:33:25 +0200

    Setting up for github pages

--- src/js/loaders.js
+++
@@ -1,9 +0,0 @@
-export function loadImage(url){
-    return new Promise(resolve =>{
-        const image = new Image();
-        image.addEventListener('load', () =>{
-            resolve(image);
-        });
-        image.src = "images/" + url;
-    })
-}
